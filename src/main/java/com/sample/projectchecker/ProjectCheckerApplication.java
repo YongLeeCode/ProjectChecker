e366a6307567a5e563f92b83940d21dcb929d03b
YongLeeCode <yonghwan311@gmail.com>
2025-05-06 17:44:59 +0900

    first project

---
+++ src/main/java/com/sample/projectchecker/ProjectCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.sample.projectchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/projectchecker/ProjectCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.projectchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
